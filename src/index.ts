e4280cf112320e554c29c517dd41eb22857c466e
Oliver Burn <oburn@atlassian.com>
2021-03-25 11:11:31 +1100

    clean up parsing arguments

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
 import { MY_CLIENT_ID, MY_CLIENT_SECRET } from './secrets'
 
 class GoogleCalendarToOrgMode extends Command {
-  static description = 'describe the command here'
+  static description = `
+  Outputs your main Google Calendar in Org mode format
+  `
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
   }
 
-  static args = [{name: 'file'}]
+  static args = [
+    {
+      name: 'file',
+      required: true,
+      description: 'Path to output the Org file (will be overwritten)'
+    }]
 
   async run() {
-    const {args, flags} = this.parse(GoogleCalendarToOrgMode)
+    const { args, flags } = this.parse(GoogleCalendarToOrgMode)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    this.log(`Running with:`)
+    this.log(`            args.file = ${args.file}`)
     this.log(`         MY_CLIENT_ID = ${MY_CLIENT_ID}`)
     this.log(`     MY_CLIENT_SECRET = ${MY_CLIENT_SECRET}`)
     this.log(`  this.config.dataDir = ${this.config.dataDir}`)
